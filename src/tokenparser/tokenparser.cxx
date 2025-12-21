20230a73dccab09bd7694695532c6559eadf232d
lahit1 <naifcanbasci@gmail.com>
2025-12-21 05:30:05 +0300

    Fix bug that causes UB Improve algorithm. Add little documentation

--- src/tokenparser/tokenparser.cxx
+++ src/tokenparser/tokenparser.cxx
@@ -175,6 +175,7 @@ namespace Tokenparser {
 		bool hasAssignments = false;
 		std::vector< std::tuple< std::string, std::shared_ptr<Typer>, ExprPtr > > var_list;
 		std::vector< std::shared_ptr<Typer> > t_list;
+
 		do {
 			if(c_token.ttype != Tokens::TOK_IDENTIFIER) {
 				break;
@@ -184,23 +185,32 @@ namespace Tokenparser {
 			eat(Tokens::TOK_IDENTIFIER);
 
 			std::shared_ptr<Typer> c_typer = std::make_shared<Typer>();
-			if(!eatTyper(c_typer, true)) {
-				/* error */
-			}
 
+
+			/**
+			 *  No need to check if it's nullptr or not by default,
+			 *  Only if it's constant
+			 */
+			eatTyper(c_typer, true);
+
+
+			ExprPtr initializer = nullptr;
 			if(eat(Tokens::TOK_ASSIGN)) {
 				hasAssignments = true;
-				ExprPtr expr = std::make_shared<BinaryExpression>(
-			                std::make_shared<VariableExpression>(var_name),
-			                OPE::ASSIGN,
-			                eval_single(Tokens::TOK_SYS_SKIP)
-			        );
-				var_list.push_back({
-					var_name,
-					c_typer,
-					expr
-				});
+
+				initializer = eval_single(Tokens::TOK_SYS_SKIP);
+				if(!initializer) {
+					/* error */
+					std::cout << "No value passed after '=' !" << std::endl;
+					return 0;
+				}
 			}
+
+			var_list.push_back({
+				var_name,
+				c_typer,
+				initializer
+			});
 
 		} while(eat(Tokens::TOK_COMMA));
 
@@ -213,23 +223,19 @@ namespace Tokenparser {
 			}
 
 			if(eat(Tokens::TOK_ASSIGN)) {
-				if(hasAssignments) {
-					/* warning */
-					// Some variables has already been assigned!
-				}
 				master_assign = eval_single(Tokens::TOK_SYS_SKIP);
 			}
 		}
 
 		int index=0;
 
-		std::shared_ptr<MultipleDeclarationStatement> mulDecStm;
+		std::shared_ptr<MultipleDeclarationStatement> mulDecStm = std::make_shared<MultipleDeclarationStatement>();
 		if(mulDecStm->initializer = master_assign) {
 			mulDecStm = std::make_shared<MultipleDeclarationStatement>();
 			parent->childs.push_back(mulDecStm);
 		}
 
-		for(auto [var_name, typer, expr]: var_list) {
+		for(auto [var_name, typer, initializer]: var_list) {
 			if(master_typer) {
 				master_typer->respect_typer = typer;
 				typer = master_typer;
@@ -237,12 +243,25 @@ namespace Tokenparser {
 
 			std::shared_ptr<DeclarationStatement> decStm = std::make_shared<DeclarationStatement>();
                         decStm->name = var_name;
-			decStm->dec_type = dec_type;
                         decStm->type_spec = typer;
+			decStm->dec_type = dec_type;
+
+			/**
+			 *  Must specify the variable type if it's const
+			 */
+			if(decStm->dec_type == DeclarationType::CONST && !typer) {
+				std::cout << "Must specify variable type for constants!" << std::endl;
+			}
+
 			if(master_assign)
-				mulDecStm->list.push_back(decStm);
+				if(initializer) {
+					/* warning */
+					// Some variables has already been assigned!
+					std::cout << "This variables has already been assigned!" << std::endl;
+				} else
+					mulDecStm->list.push_back(decStm);
 			else {
-				decStm->initializer = expr;
+				decStm->initializer = initializer;
 	                        parent->childs.push_back(decStm);
 			}
 		}
