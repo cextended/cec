2eb625c4c4fe2e0ad26493cd629ab39071f3ad97
lahit1 <naifcanbasci@gmail.com>
2025-12-26 04:17:19 +0300

    Add DataPipe struct because of inefficent and non-portable pipeline system( Unix pipes aren't portable for non-unix systems and having more than one executable is mostly unnecessary )

---
+++ include/util/datapipe.hxx
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <queue>
+#include <condition_variable>
+#include<mutex>
+
+template<typename T>
+struct DataPipe {
+
+	std::condition_variable con_var;
+
+	std::mutex mut;
+	std::queue<T> data;
+
+	inline DataPipe() = default;
+	inline DataPipe(const DataPipe&) = delete;
+	inline DataPipe& operator=(const DataPipe&) = delete;
+
+	inline DataPipe<T>& operator<<(T &&value) {
+		std::unique_lock<std::mutex> lock(mut);
+
+		this->data.push(std::move(value));
+
+		con_var.notify_one();
+		return *this;
+	}
+
+	inline DataPipe<T>& operator>>(T &value) {
+		std::unique_lock<std::mutex> lock(mut);
+		while(data.empty())
+			con_var.wait(lock);
+
+		value = std::move(this->data.front());
+		this->data.pop();
+
+		return *this;
+	}
+
+};
