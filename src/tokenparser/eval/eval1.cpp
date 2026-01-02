5c7135b4893466d6b25f2be7e7bf9dd036040e9c
lahit1 <naifcanbasci@gmail.com>
2026-01-02 08:50:30 +0300

    Fix the bug that causes evualating the operators with wrong order

--- src/tokenparser/eval/eval1.cpp
+++ src/tokenparser/eval/eval1.cpp
@@ -5,17 +5,19 @@ namespace Tokenparser {
 		ExprPtr main_expr = eval2();
 		if(!main_expr) return 0;
 
-		if(eat(Tokens::TOK_PLUS))
-			return std::make_shared<BinaryExpression>(
-						main_expr,
-						OPE::ADD,
-						eval1());
-		else if(eat(Tokens::TOK_MINUS))
-			return std::make_shared<BinaryExpression>(
-						main_expr,
-						OPE::SUB,
-						eval1());
-
+		while(true) {
+			if(eat(Tokens::TOK_PLUS))
+				main_expr = std::make_shared<BinaryExpression>(
+							main_expr,
+							OPE::ADD,
+							eval2());
+			else if(eat(Tokens::TOK_MINUS))
+				main_expr = std::make_shared<BinaryExpression>(
+							main_expr,
+							OPE::SUB,
+							eval2());
+			else break;
+		}
 		return main_expr;
 	}
 }
--- src/tokenparser/eval/eval2.cpp
+++ src/tokenparser/eval/eval2.cpp
@@ -5,22 +5,24 @@ namespace Tokenparser {
 		ExprPtr main_expr = eval3();
 		if(!main_expr) return 0;
 
-		if(eat(Tokens::TOK_STAR))
-			return std::make_shared<BinaryExpression>(
-						main_expr,
-						OPE::MUL,
-						eval2());
-		else if(eat(Tokens::TOK_SLASH))
-			return std::make_shared<BinaryExpression>(
-						main_expr,
-						OPE::DIV,
-						eval2());
-		else if(eat(Tokens::TOK_MOD))
-			return std::make_shared<BinaryExpression>(
-						main_expr,
-						OPE::MOD,
-						eval2());
-
+		while(true) {
+			if(eat(Tokens::TOK_STAR))
+				main_expr = std::make_shared<BinaryExpression>(
+							main_expr,
+							OPE::MUL,
+							eval3());
+			else if(eat(Tokens::TOK_SLASH))
+				main_expr = std::make_shared<BinaryExpression>(
+							main_expr,
+							OPE::DIV,
+							eval3());
+			else if(eat(Tokens::TOK_MOD))
+				main_expr = std::make_shared<BinaryExpression>(
+							main_expr,
+							OPE::MOD,
+							eval3());
+			else break;
+		}
 		return main_expr;
 	}
 }
