f0c7461886f930deb2e5d9b56e43fbacfc36a1fe
lahit1 <naifcanbasci@gmail.com>
2026-01-22 14:01:44 +0300

    Make sure it's inlined and don't forget the '#pragma once'

--- include/segvc/util/concat.hxx
+++ include/segvc/util/concat.hxx
@@ -1,8 +1,14 @@
-void concat(std::shared_ptr<Typer> start, std::shared_ptr<Typer> end) {
-	std::shared_ptr<Typer> current_end = start;
-	while(current_end->respect_typer) {
-		current_end = current_end->respect_typer;
+#pragma once
+
+namespace segvc {
+
+	inline void concat(std::shared_ptr<Typer> start, std::shared_ptr<Typer> end) {
+		std::shared_ptr<Typer> current_end = start;
+		while(current_end->respect_typer) {
+			current_end = current_end->respect_typer;
+		}
+
+		current_end->respect_typer = end;
 	}
 
-	current_end->respect_typer = end;
 }
